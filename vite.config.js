c9607908aea361f0a3af9cbdf6bd5c7cb429bb61
Arrizo <33471210+Arrizo@users.noreply.github.com>
2025-12-15 11:35:47 +0800

    项目的初始化
    react项目的初始化和基础搭建

---
+++ vite.config.js
@@ -0,0 +1,56 @@
+/*
+ * @Author: chase
+ * @Date: 2025-08-06 14:32:19
+ * @LastEditors: chase
+ * @LastEditTime: 2025-10-31 15:03:09
+ * @FilePath: \react\reacti-project\vite.config.js
+ * @Description: 
+ * 
+ */
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import process from 'node:process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+// https://vite.dev/config/
+
+
+export default ({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd())
+  const proxyPrefix = env.VITE_API_BASE_URL
+  return defineConfig({
+    plugins: [react()],
+    base: './',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          relativeUrls: true,
+          modifyVars: {
+          }
+        }
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      open: true,
+      proxy: {
+        [proxyPrefix]: {
+          target: env.VITE_APP_API_BASEURL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
+
+  })
+}
+
+
